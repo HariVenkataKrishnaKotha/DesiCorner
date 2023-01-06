3cf63e6fb52096df8346b6fba837fdca1f9672c9
hvkkotha <hk00016@mix.wvu.edu>
2023-01-06 09:01:17 -0500

    Added basic controller class and view page to test the application functionality

---
+++ Controllers/DishController.cs
@@ -0,0 +1,26 @@
+﻿using DesiCorner.Models;
+using DesiCorner.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesiCorner.Controllers
+{
+    public class DishController : Controller
+    {
+        private readonly IDishRepository _dishRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public DishController(IDishRepository dishRepository, ICategoryRepository categoryRepository)
+        {
+            _dishRepository = dishRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult List()
+        {
+            //ViewBag.CurrentCategory = "Biriyani";
+            //return View(_dishRepository.AllDishes);
+            DishListViewModel dishListViewModel = new DishListViewModel(_dishRepository.AllDishes, "Biriyani");
+            return View(dishListViewModel);
+        }
+    }
+}
---
+++ ViewModels/DishListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using DesiCorner.Models;
+
+namespace DesiCorner.ViewModels
+{
+    public class DishListViewModel
+    {
+        public IEnumerable<Dish> Dishes { get; }
+        public string? CurrentCategory { get; }
+
+        public DishListViewModel(IEnumerable<Dish> dishes, string? currenCategory)
+        {
+            Dishes = dishes;
+            CurrentCategory = currenCategory;
+        }
+    }
+}
